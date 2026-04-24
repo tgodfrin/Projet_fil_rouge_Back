64c101945adcf77be3f65aa055fcaf3c9729b0ac
Thomas GODFRIN <thomas.godfrin98@gmail.com>
2026-04-24 15:19:06 +0200

    feat(equipment-family): add JsonView annotations and controller

--- src/main/java/com/locmns/model/EquipmentFamily.java
+++ src/main/java/com/locmns/model/EquipmentFamily.java
@@ -1,5 +1,9 @@
 package com.locmns.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.locmns.view.EquipmentFamilyView;
+import com.locmns.view.EquipmentView;
+import com.locmns.view.LoanView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -17,10 +21,12 @@ public class EquipmentFamily {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView({EquipmentFamilyView.class, EquipmentView.class, LoanView.class})
     protected Integer id;
 
-    @Column(length=30,nullable = false,unique = true)
+    @Column(length = 30, nullable = false, unique = true)
     @NotBlank
-    @Size(min=3,max=30)
+    @Size(min = 3, max = 30)
+    @JsonView({EquipmentFamilyView.class, EquipmentView.class})
     protected String nameEquipmentFamily;
 }
